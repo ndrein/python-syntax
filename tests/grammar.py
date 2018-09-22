87c51afccc222c9452d42a0f91c1d9272cf9e171
Noah Reinhardt <ndrein@gmail.com>
2018-09-21 20:55:34 -0400

    Add requirements.txt

--- tests/grammar.py
+++ tests/grammar.py
@@ -1,16 +1,21 @@
+from collections import namedtuple
+
 from tests.character_not_in_alphabet import CharacterNotInAlphabet
+
+Token = namedtuple('Token', ['TokenType', 's'])
 
 
 class Grammar:
     def __init__(self, states, start_state, accept_states, alphabet, transitions, accept_state_to_token_type):
         self.alphabet = alphabet
+        self.accept_state_to_token_type = accept_state_to_token_type
 
     def tokenize(self, s):
         if s == '':
-            return '', ''
+            return None, ''
         elif self.alphabet == set():
             raise CharacterNotInAlphabet
         elif set(s) != self.alphabet:
             raise CharacterNotInAlphabet
         else:
-            return s[0], s[1:]
+            return Token(self.accept_state_to_token_type[2], [0]), s[1:]
