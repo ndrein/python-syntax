cb33ba0423dc75129d9dcb359e49e8dca4fac7ba
Noah Reinhardt <ndrein@gmail.com>
2018-09-29 16:40:12 -0400

    Add aa test

--- tests/test_two_characters.py
+++ tests/test_two_characters.py
@@ -8,3 +8,7 @@ grammar = Grammar({0, 1}, 0, {0, 1}, {'a', 'b'}, {(0, 'a'): 0, (0, 'b'): 1}, {0:
 
 def test_a():
     assert grammar.munch('a') == (Token(TokenType.A, 'a'), '')
+
+
+def test_aa():
+    assert grammar.munch('aa') == (Token(TokenType.A, 'aa'), '')
