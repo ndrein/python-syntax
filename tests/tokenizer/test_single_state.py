97a12cb33573100814bbaf1f1c8295ccd11676d7
Noah Reinhardt <ndrein@gmail.com>
2018-10-21 21:57:19 -0400

    Remove broken Tokenizer methods

--- tests/tokenizer/test_single_state.py
+++
@@ -1,7 +0,0 @@
-from syntax.dfa import DFA
-
-tokenizer = DFA(start_state=0, accept_states=set(), alphabet={'a'}, transitions={(0, 'a'): 0})
-
-
-def test_a():
-    assert (None, 'a') == tokenizer.traverse('a')
