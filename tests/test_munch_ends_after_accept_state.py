44df50deb6fa9701aea82b782cf9bdbe8db3d2ec
Noah Reinhardt <ndrein@gmail.com>
2018-10-30 21:16:51 -0400

    Add failing test for emitting token if transition doesn't exist

--- tests/test_munch_ends_after_accept_state.py
+++ tests/test_munch_ends_after_accept_state.py
@@ -14,3 +14,8 @@ def test_start_state_is_accept_state():
 def test_munch_ends_after_nonempty_token():
     tokenizer = Tokenizer({(0, 'a'): 1, (1, 'a'): 2}, 0, {1}, {1: TokenType.A})
     assert Token(TokenType.A, 'a'), 1 == tokenizer.munch('aa')
+
+
+def test_munch_ends_if_no_transition_exists():
+    tokenizer = Tokenizer({(0, 'a'): 1}, 0, {1}, {1: TokenType.A})
+    assert Token(TokenType.A, 'a') == tokenizer.munch('aa')
