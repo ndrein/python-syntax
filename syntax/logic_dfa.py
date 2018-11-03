d2f5c3a685c542e9513fc0afe5e0f0cbeda3ef7e
Noah Reinhardt <ndrein@gmail.com>
2018-11-03 17:38:37 -0400

    Refactor

--- syntax/logic_dfa.py
+++ syntax/logic_dfa.py
@@ -1,14 +1,21 @@
 from enum import Enum
+from itertools import count
 from string import ascii_lowercase
 
 TokenType = Enum('TokenType', ['LITERAL', 'NOT', 'AND', 'OR', 'IMPLIES', 'LPAREN', 'RPAREN', 'WHITESPACE'])
-
 LOGIC_DFA = {}
+counter = count(0)
+
+START = 0
+LITERAL = 1
+NOT_1 = 2
+NOT_2 = 3
+NOT_3 = 4
 
 for a in ascii_lowercase:
-    LOGIC_DFA[0, a] = 1
-    LOGIC_DFA[1, a] = 1
+    LOGIC_DFA[START, a] = LITERAL
+    LOGIC_DFA[LITERAL, a] = LITERAL
 
-LOGIC_DFA[0, 'N'] = 2
-LOGIC_DFA[2, 'O'] = 3
-LOGIC_DFA[3, 'T'] = 4
+LOGIC_DFA[START, 'N'] = NOT_1
+LOGIC_DFA[NOT_1, 'O'] = NOT_2
+LOGIC_DFA[NOT_2, 'T'] = NOT_3
