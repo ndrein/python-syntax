1f18c8a869493953085fc365d8fed6836844d6e9
Noah Reinhardt <ndrein@gmail.com>
2018-11-03 11:48:26 -0400

    Add test for throwing ValueError if a nonempty string does not result in a tokenization

--- tests/test_tokenize.py
+++ tests/test_tokenize.py
@@ -20,3 +20,8 @@ def test_two_tokens():
     TokenType = Enum('TokenType', ['A'])
     tokenizer = Tokenizer({(0, 'a'): 1}, 0, {1}, {1: TokenType.A})
     assert [Token(TokenType.A, 'a'), Token(TokenType.A, 'a')] == tokenizer.tokenize('aa')
+
+
+def test_nonempty_string_gives_token_not_formed():
+    with pytest.raises(ValueError):
+        Tokenizer({(0, 'a'): 1}, 0, set(), {}).tokenize('a')
