ec4d81e112b861f17c0a47669630fe26b903074a
Noah Reinhardt <ndrein@gmail.com>
2018-11-03 18:50:44 -0400

    Write implementation for DFA of AND OR AND IMPLIES

--- syntax/logic_dfa.py
+++ syntax/logic_dfa.py
@@ -6,11 +6,23 @@ TokenType = Enum('TokenType', ['LITERAL', 'NOT', 'AND', 'OR', 'IMPLIES', 'LPAREN
 LOGIC_DFA = {}
 counter = count(0)
 
-START = 0
-LITERAL = 1
-NOT_1 = 2
-NOT_2 = 3
-NOT_3 = 4
+START = next(counter)
+LITERAL = next(counter)
+NOT_1 = next(counter)
+NOT_2 = next(counter)
+NOT_3 = next(counter)
+AND_1 = next(counter)
+AND_2 = next(counter)
+AND_3 = next(counter)
+OR_1 = next(counter)
+OR_2 = next(counter)
+IMPLIES_1 = next(counter)
+IMPLIES_2 = next(counter)
+IMPLIES_3 = next(counter)
+IMPLIES_4 = next(counter)
+IMPLIES_5 = next(counter)
+IMPLIES_6 = next(counter)
+IMPLIES_7 = next(counter)
 
 for a in ascii_lowercase:
     LOGIC_DFA[START, a] = LITERAL
@@ -19,3 +31,15 @@ for a in ascii_lowercase:
 LOGIC_DFA[START, 'N'] = NOT_1
 LOGIC_DFA[NOT_1, 'O'] = NOT_2
 LOGIC_DFA[NOT_2, 'T'] = NOT_3
+
+LOGIC_DFA[START, 'A'] = AND_1
+LOGIC_DFA[AND_1, 'N'] = AND_2
+LOGIC_DFA[AND_2, 'D'] = AND_3
+
+LOGIC_DFA[START, 'I'] = IMPLIES_1
+LOGIC_DFA[IMPLIES_1, 'M'] = IMPLIES_2
+LOGIC_DFA[IMPLIES_2, 'P'] = IMPLIES_3
+LOGIC_DFA[IMPLIES_3, 'L'] = IMPLIES_4
+LOGIC_DFA[IMPLIES_4, 'I'] = IMPLIES_5
+LOGIC_DFA[IMPLIES_5, 'E'] = IMPLIES_6
+LOGIC_DFA[IMPLIES_6, 'S'] = IMPLIES_7
