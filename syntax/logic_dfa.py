ec0c4dc210abb8ede8b2eeafd42219774ea60b9d
Noah Reinhardt <ndrein@gmail.com>
2018-11-24 14:21:57 -0500

    Compress logic_dfa states into a list

--- syntax/logic_dfa.py
+++ syntax/logic_dfa.py
@@ -8,21 +8,10 @@ counter = count(0)
 
 START = next(counter)
 LITERAL = next(counter)
-NOT_1 = next(counter)
-NOT_2 = next(counter)
-NOT_3 = next(counter)
-AND_1 = next(counter)
-AND_2 = next(counter)
-AND_3 = next(counter)
-OR_1 = next(counter)
-OR_2 = next(counter)
-IMPLIES_1 = next(counter)
-IMPLIES_2 = next(counter)
-IMPLIES_3 = next(counter)
-IMPLIES_4 = next(counter)
-IMPLIES_5 = next(counter)
-IMPLIES_6 = next(counter)
-IMPLIES_7 = next(counter)
+NOT = [next(counter) for _ in 'NOT']
+AND = [next(counter) for _ in 'AND']
+OR = [next(counter) for _ in 'OR']
+IMPLIES = [next(counter) for _ in 'IMPLIES']
 LPAREN = next(counter)
 RPAREN = next(counter)
 SPACE = next(counter)
@@ -31,24 +20,24 @@ for a in ascii_lowercase:
     LOGIC_DFA[START, a] = LITERAL
     LOGIC_DFA[LITERAL, a] = LITERAL
 
-LOGIC_DFA[START, 'N'] = NOT_1
-LOGIC_DFA[NOT_1, 'O'] = NOT_2
-LOGIC_DFA[NOT_2, 'T'] = NOT_3
+LOGIC_DFA[START, 'N'] = NOT[0]
+LOGIC_DFA[NOT[0], 'O'] = NOT[1]
+LOGIC_DFA[NOT[1], 'T'] = NOT[2]
 
-LOGIC_DFA[START, 'A'] = AND_1
-LOGIC_DFA[AND_1, 'N'] = AND_2
-LOGIC_DFA[AND_2, 'D'] = AND_3
+LOGIC_DFA[START, 'A'] = AND[0]
+LOGIC_DFA[AND[0], 'N'] = AND[1]
+LOGIC_DFA[AND[1], 'D'] = AND[2]
 
-LOGIC_DFA[START, 'O'] = OR_1
-LOGIC_DFA[OR_1, 'R'] = OR_2
+LOGIC_DFA[START, 'O'] = OR[0]
+LOGIC_DFA[OR[0], 'R'] = OR[1]
 
-LOGIC_DFA[START, 'I'] = IMPLIES_1
-LOGIC_DFA[IMPLIES_1, 'M'] = IMPLIES_2
-LOGIC_DFA[IMPLIES_2, 'P'] = IMPLIES_3
-LOGIC_DFA[IMPLIES_3, 'L'] = IMPLIES_4
-LOGIC_DFA[IMPLIES_4, 'I'] = IMPLIES_5
-LOGIC_DFA[IMPLIES_5, 'E'] = IMPLIES_6
-LOGIC_DFA[IMPLIES_6, 'S'] = IMPLIES_7
+LOGIC_DFA[START, 'I'] = IMPLIES[0]
+LOGIC_DFA[IMPLIES[0], 'M'] = IMPLIES[1]
+LOGIC_DFA[IMPLIES[1], 'P'] = IMPLIES[2]
+LOGIC_DFA[IMPLIES[2], 'L'] = IMPLIES[3]
+LOGIC_DFA[IMPLIES[3], 'I'] = IMPLIES[4]
+LOGIC_DFA[IMPLIES[4], 'E'] = IMPLIES[5]
+LOGIC_DFA[IMPLIES[5], 'S'] = IMPLIES[6]
 
 LOGIC_DFA[START, '('] = LPAREN
 
