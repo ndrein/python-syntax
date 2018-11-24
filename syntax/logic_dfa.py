fb4a95212950c18fa9e74f140ab7e8ef0d63d8e4
Noah Reinhardt <ndrein@gmail.com>
2018-11-24 14:35:32 -0500

    test_logic_dfa is really testing the logic dfa transitions

--- syntax/logic_dfa.py
+++ syntax/logic_dfa.py
@@ -3,7 +3,7 @@ from itertools import count
 from string import ascii_lowercase
 
 TokenType = Enum('TokenType', ['LITERAL', 'NOT', 'AND', 'OR', 'IMPLIES', 'LPAREN', 'RPAREN', 'SPACE'])
-logic_dfa = {}
+transitions = {}
 counter = count(0)
 
 START = next(counter)
@@ -17,31 +17,36 @@ RPAREN = next(counter)
 SPACE = next(counter)
 
 for a in ascii_lowercase:
-    logic_dfa[START, a] = LITERAL
-    logic_dfa[LITERAL, a] = LITERAL
+    transitions[START, a] = LITERAL
+    transitions[LITERAL, a] = LITERAL
 
-logic_dfa[START, 'N'] = NOT[0]
-logic_dfa[NOT[0], 'O'] = NOT[1]
-logic_dfa[NOT[1], 'T'] = NOT[2]
+transitions[START, 'N'] = NOT[0]
+transitions[NOT[0], 'O'] = NOT[1]
+transitions[NOT[1], 'T'] = NOT[2]
 
-logic_dfa[START, 'A'] = AND[0]
-logic_dfa[AND[0], 'N'] = AND[1]
-logic_dfa[AND[1], 'D'] = AND[2]
+transitions[START, 'A'] = AND[0]
+transitions[AND[0], 'N'] = AND[1]
+transitions[AND[1], 'D'] = AND[2]
 
-logic_dfa[START, 'O'] = OR[0]
-logic_dfa[OR[0], 'R'] = OR[1]
+transitions[START, 'O'] = OR[0]
+transitions[OR[0], 'R'] = OR[1]
 
-logic_dfa[START, 'I'] = IMPLIES[0]
-logic_dfa[IMPLIES[0], 'M'] = IMPLIES[1]
-logic_dfa[IMPLIES[1], 'P'] = IMPLIES[2]
-logic_dfa[IMPLIES[2], 'L'] = IMPLIES[3]
-logic_dfa[IMPLIES[3], 'I'] = IMPLIES[4]
-logic_dfa[IMPLIES[4], 'E'] = IMPLIES[5]
-logic_dfa[IMPLIES[5], 'S'] = IMPLIES[6]
+transitions[START, 'I'] = IMPLIES[0]
+transitions[IMPLIES[0], 'M'] = IMPLIES[1]
+transitions[IMPLIES[1], 'P'] = IMPLIES[2]
+transitions[IMPLIES[2], 'L'] = IMPLIES[3]
+transitions[IMPLIES[3], 'I'] = IMPLIES[4]
+transitions[IMPLIES[4], 'E'] = IMPLIES[5]
+transitions[IMPLIES[5], 'S'] = IMPLIES[6]
 
-logic_dfa[START, '('] = LPAREN
+transitions[START, '('] = LPAREN
 
-logic_dfa[START, ')'] = RPAREN
+transitions[START, ')'] = RPAREN
 
-logic_dfa[START, ' '] = SPACE
-logic_dfa[SPACE, ' '] = SPACE
+transitions[START, ' '] = SPACE
+transitions[SPACE, ' '] = SPACE
+
+#
+# def get_logic_dfa():
+#     return transitions, START, {LITERAL, NOT[-1], AND[-1], OR[-1], IMPLIES[-1], LPAREN, RPAREN, SPACE},
+# {LITERAL: TokenType.LITERAL, NOT[-1]: TokenType.NOT, AND[-]}
