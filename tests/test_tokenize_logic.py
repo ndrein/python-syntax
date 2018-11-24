f6cf3d70863af80e8e5f891a61c4efb6cc988ab0
Noah Reinhardt <ndrein@gmail.com>
2018-11-24 17:21:23 -0500

    Add tests for some logic tokens

--- tests/test_tokenize_logic.py
+++ tests/test_tokenize_logic.py
@@ -1,7 +1,27 @@
-from syntax.logic_dfa import logic_dfa
-from syntax.tokenizer import Tokenizer
+from syntax.logic_dfa import logic_dfa, TokenType
+from syntax.tokenizer import Tokenizer, Token
+
+tokenizer = Tokenizer(logic_dfa.transitions, logic_dfa.start_state, logic_dfa.accept_states,
+                      logic_dfa.accept_state_to_token_type)
 
 
-def test_simple_formula():
-    tokenizer = Tokenizer(logic_dfa.transitions, logic_dfa.start_state, logic_dfa.accept_states,
-                          logic_dfa.accept_state_to_token_type)
+def test_space():
+    assert [Token(TokenType.SPACE, ' ')] == tokenizer.tokenize(' ')
+
+
+def test_literal():
+    assert [Token(TokenType.LITERAL, 'a')] == tokenizer.tokenize('a')
+
+
+def test_literal_and_operator():
+    assert [Token(TokenType.LITERAL, 'a'), Token(TokenType, ' ')] == tokenizer.tokenize('a ')
+
+
+def test_lowercase_operators():
+    not_ = Token(TokenType.LITERAL, 'not')
+    space = Token(TokenType.SPACE, ' ')
+    and_ = Token(TokenType.LITERAL, 'and')
+    or_ = Token(TokenType.LITERAL, 'or')
+    implies = Token(TokenType.LITERAL, 'implies')
+
+    assert [not_, space, and_, space, or_, space, implies] == tokenizer.tokenize('not and or implies')
