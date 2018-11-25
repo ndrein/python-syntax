3dd2ae1de3366d7f4482d64de3b4d0e03634d420
Noah Reinhardt <ndrein@gmail.com>
2018-11-24 23:30:51 -0500

    Remove duplicate tests

--- tests/tokenizer/test_tokenize.py
+++ tests/tokenizer/test_tokenize.py
@@ -5,7 +5,7 @@ import pytest
 from syntax.tokenizer import Tokenizer, Token
 
 
-def test_empty_string_raises_error():
+def test_empty_string_gives_no_tokens():
     assert [] == Tokenizer({}, 0, set(), {}).tokenize('')
 
 
@@ -53,25 +53,14 @@ def test_munch_ends_if_no_transition_exists():
     assert [Token(TokenType.A, 'a'), Token(TokenType.A, 'a')] == tokenizer.tokenize('aa')
 
 
-def test_two_tokens():
-    TokenType = Enum('TokenType', ['A'])
-    tokenizer = Tokenizer({(0, 'a'): 1}, 0, {1}, {1: TokenType.A})
-    assert [Token(TokenType.A, 'a'), Token(TokenType.A, 'a')] == tokenizer.tokenize('aa')
-
-
-def test_nonempty_string_gives_token_not_formed():
-    with pytest.raises(ValueError):
-        Tokenizer({(0, 'a'): 1}, 0, set(), {}).tokenize('a')
-
-
 def test_two_different_tokens():
     TokenType = Enum('TokenType', ['A', 'B'])
     tokenizer = Tokenizer({(0, 'a'): 1, (0, 'b'): 2}, 0, {1, 2}, {1: TokenType.A, 2: TokenType.B})
     assert [Token(TokenType.A, 'a'), Token(TokenType.B, 'b')] == tokenizer.tokenize('ab')
 
 
-def test_three_tokens():
+def test_many_tokens():
     TokenType = Enum('TokenType', ['A'])
     tokenizer = Tokenizer({(0, 'a'): 1}, 0, {1}, {1: TokenType.A})
     token = Token(TokenType.A, 'a')
-    assert [token] * 3 == tokenizer.tokenize('aaa')
+    assert [token] * 1000 == tokenizer.tokenize('a' * 1000)
