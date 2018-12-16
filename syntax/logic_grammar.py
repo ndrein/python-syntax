206980cc9264ddae54b05abe5ca5766757e97514
Noah Reinhardt <ndrein@gmail.com>
2018-12-15 19:37:31 -0500

    Add tests for not and or

--- syntax/logic_grammar.py
+++ syntax/logic_grammar.py
@@ -1,5 +1,10 @@
 LOGIC_GRAMMAR = '''
-statement: /[a-z]+/ 
+statement: LITERAL
+         | not
+         | or
+LITERAL: /[a-z]+/
+not: "(NOT" statement ")"
+or: "(" statement "OR" statement ")"
 
 %import common.WS
 %ignore WS
--- tests/test_logic_grammar.py
+++ tests/test_logic_grammar.py
@@ -1,5 +1,8 @@
+from typing import List
+
 import pytest
 from lark import Lark
+from lark import Tree
 from lark.exceptions import UnexpectedCharacters, ParseError
 
 from syntax.logic_grammar import LOGIC_GRAMMAR
@@ -37,3 +40,33 @@ def test_unexpected_characters():
 def test_space_gives_parse_error():
     with pytest.raises(ParseError):
         grammar.parse(' ')
+
+
+def test_literal_with_whitespace():
+    assert ['p'] == grammar.parse('  p ').children
+
+
+def single_connective_test(root: Tree, rule_name: str, literals: List[str]):
+    rule_node = root.children[0]
+    assert rule_name == rule_node.data
+    for literal, statement in zip(literals, rule_node.children):
+        assert literal == statement.children[0]
+
+
+def test_not_without_whitespace():
+    single_connective_test(grammar.parse('(NOTp)'), 'not', ['p'])
+
+
+def test_not_with_whitespace():
+    single_connective_test(grammar.parse('  (NOT p ) '), 'not', ['p'])
+
+
+def test_double_negation():
+    tree = grammar.parse('(NOT (NOT p))')
+    not_node_1 = tree.children[0]
+    assert 'not' == not_node_1.data
+    single_connective_test(not_node_1.children[0], 'not', ['p'])
+
+
+def test_or():
+    single_connective_test(grammar.parse('(pORq)'), 'or', ['p', 'q'])
